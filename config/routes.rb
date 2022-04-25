195e7d2b063114b06aa88a073ff79773f42654a7
sakikitaori <kitaori.40394@gmail.com>
2022-04-25 18:23:04 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
+
+  root to: "new_user_session"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
